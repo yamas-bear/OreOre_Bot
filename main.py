1eb25c93c1c87e55d00c60597eb5674d90f932c4
kodai <2110koudai@gmail.com>
2020-12-29 16:36:15 +0900

    選択する記事をランダムに選択するように変更

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ CHANNEL_ACCESS_TOKEN = info['CHANNEL_ACCESS_TOKEN']
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 data = requests.get('https://note.com/api/v2/notes')
-name = data.json()['data']['contents'][0]['name']
-username = data.json()['data']['contents'][0]['user']['name']
-urlname = data.json()['data']['contents'][0]['user']['urlname']
-key = data.json()['data']['contents'][0]['key']
+data_number = random.randint(0,20)
+name = data.json()['data']['contents'][data_number]['name']
+username = data.json()['data']['contents'][data_number]['user']['name']
+urlname = data.json()['data']['contents'][data_number]['user']['urlname']
+key = data.json()['data']['contents'][data_number]['key']
 
 def main():
     USER_ID = info['USER_ID']
