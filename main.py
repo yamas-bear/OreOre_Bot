59b5d231d9a903be4a56c2e1e82c6b3781fe3a4f
kodai <2110koudai@gmail.com>
2020-12-31 16:16:05 +0900

    qiitaの記事の取得を実装

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import random
+import main.py
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
@@ -21,10 +22,15 @@ username = data['user']['name']
 urlname = data['user']['urlname']
 key = data['key']
 
+#qiitaのデータを取得
+qiita_data = get_qiita_trend_data()
+qiita_title = qiita_data[0]
+qiita_url = qiita_data[1]
+
 def main():
     USER_ID = info['USER_ID']
 #     messages = TextSendMessage(text="おっはモーニング\nもう無理限界。\n牛丼の出汁がきいたあの香りにつられて吉野屋に来た。")
-    messages = TextSendMessage(text="おっはモーニング\n今日のオススメ記事だよ。\n「{}-{}」\nhttps://note.com/{}/n/{}".format(name,username,urlname,key))
+    messages = TextSendMessage(text="おっはモーニング\n今日のオススメ記事だよ。\n「{}-{}」\nhttps://note.com/{}/n/{}\n「{}」\n{}".format(name,username,urlname,key,qiita_title,qiita_url))
     line_bot_api.push_message(USER_ID,messages=messages)
 
 if __name__ == "__main__":
---
+++ qiita.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+def get_qiita_trend_data:
+    url = 'https://qiita.com/'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text,"html.parser")
+    data = soup.find(class_="css-81mxb5")
+    data = a.find('h2')
+    data = a.find('a')
+    #記事のタイトル取得
+    title = data.string
+    #記事のurlを取得
+    url = data['href']
+    return title,url
